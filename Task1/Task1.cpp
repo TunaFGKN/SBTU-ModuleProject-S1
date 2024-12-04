aab38802753ad53d79906437512bd85c3c493e8d
Tuna F. <swaxztn@gmail.com>
2024-12-04 16:56:53 +0300

    04.12

--- Task1/Task1.cpp
+++ Task1/Task1.cpp
@@ -9,6 +9,7 @@
 #include <iomanip>
 #include <string>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
@@ -16,6 +17,7 @@ using namespace std;
 const float GRAVITY = 9.8;
 const float PI = 3.14159;
 float initialSpeed, angle, height, ftime, _distance;
+float radian;
 
 // Function that returns the bigger root of a quadratic equation. Format: ax^2 + bx + c = 0 
 float solveForQuadraticEquation(float a, float b, float c) {
@@ -35,11 +37,15 @@ float solveForQuadraticEquation(float a, float b, float c) {
 
 // Returns the flight time by solving the quadratic equation 
 float findTheFlightTime() {
-	float radian = angle * PI / 180;
 	float a = GRAVITY / 2;
 	float b = initialSpeed * sin(radian);
 	float c = -height;
 	return solveForQuadraticEquation(a, b, c);
+}
+
+vector<float> positionVectorOverTime(float t) {
+	vector<float> component = { initialSpeed * cos(radian) * t, (-initialSpeed * sin(radian) * t) + (-GRAVITY / 2 * t * t) };
+	return component;
 }
 
 int main()
@@ -49,30 +55,37 @@ int main()
 
 	cout << "Please enter the slope angle of the roof (degree): ";
 	cin >> angle;
+	radian = angle * PI / 180;
 
 	cout << "Please enter the height (m): ";
 	cin >> height;
 
 	ftime = findTheFlightTime();
-	_distance = (initialSpeed * cos(angle * PI / 180)) * ftime;
+	_distance = (initialSpeed * cos(radian)) * ftime;
+
+	cout << "------------------------------------------------------------------------------------------------------" << endl;
+	cout << "1) Display the time of flight.\n2) Find the impact point.\n3) Horizontal and vertical coordinates of the snowball's motion over time.\n4) Decide if the snowball will hit the person or not.\n5) Exact distance at which the snowball will hit a person standing at a specified distance and height.\n9) Exit\nHello, please choose the option that you want to perform. Enter '9' to exit the program." << endl;
+	cout << "------------------------------------------------------------------------------------------------------\n" << endl;
 
 	while (true) {
 		char option;
-		cout << "------------------------------------------------------------------------------------------------------" << endl;
-		cout << "1) Display the time of flight.\n2) Find the impact point.\n3) Horizontal and vertical coordinates of the snowball's motion over time.\n4) Decide if the snowball will hit the person or not.\n5) Exact distance at which the snowball will hit a person standing at a specified distance and height.\nq) Exit\nHello, please choose the option that you want to perform. Enter 'q' to exit the program." << endl;
-		cout << "------------------------------------------------------------------------------------------------------\n" << endl;
+		
 		cin >> option;
 
 		switch (option)
 		{
 			case '1': // Flight time
-				cout << "Flight time is " << ftime << " seconds.\n" << endl;
+				cout << fixed << setprecision(3) << "Flight time is " << ftime << " seconds.\n" << endl;
 				break;
 			case '2': // Impact point
 				cout << fixed << setprecision(3) << "The ball will hit the ground " << _distance << " meters away from the edge of the barn.\n" << endl;
 				break;
 			case '3': // 
-				cout << "\n" << endl;
+				cout << ftime * 0.25 << " seconds: R = (" << positionVectorOverTime(ftime * 0.25)[0] << " i " << positionVectorOverTime(ftime * 0.25)[1] << " j) m" << endl;
+				cout << ftime * 0.5 << " seconds: R = (" << positionVectorOverTime(ftime * 0.5)[0] << " i " << positionVectorOverTime(ftime * 0.5)[1] << " j) m" << endl;
+				cout << ftime * 0.75 << " seconds: R = (" << positionVectorOverTime(ftime * 0.75)[0] << " i " << positionVectorOverTime(ftime * 0.75)[1] << " j) m" << endl;
+				cout << ftime << " seconds: R = (" << positionVectorOverTime(ftime)[0] << " i " << positionVectorOverTime(ftime)[1] << " j) m" << endl;
+				cout << endl;
 				break;
 			case '4': // 
 				cout << "\n" << endl;
@@ -80,7 +93,7 @@ int main()
 			case '5': // 
 				cout << "\n" << endl;
 				break;
-			case 'q': //
+			case '9': //
 				cout << "Closing the program...\n" << endl;
 				return 0;
 			default:
